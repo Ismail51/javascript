25f7ddb14fbfdc7d4d0f1afa62fc4c2183a2fde5
Ismail51 <dassler-ben@hotmail.fr>
2022-01-31 15:12:16 +0100

    exo 2 done

---
+++ jour04/jour04.js
@@ -0,0 +1,25 @@
+// 01 object
+let cat = {
+    name: "Garfield",
+    age: 3,
+    isCute: true,
+}
+console.log(cat);
+console.log(cat.age);
+if(cat.isCute === true){
+    console.log("so cute !")
+};
+
+// 02 Combine
+let cat2 = {
+    name: "bibo",
+    age: 5, 
+    isCute: true,
+}
+let cats = [
+    cat,cat2
+];
+//cats[0].age;
+console.log(cats[0].age)
+    
+
