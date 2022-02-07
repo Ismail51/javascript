df093c375f251200dbf4c9d784e13ab28cd84a13
Ismail51 <dassler-ben@hotmail.fr>
2022-02-07 16:37:48 +0100

    exo 1

---
+++ jour08/jour08.js
@@ -0,0 +1,19 @@
+// 01 countries
+const request = require("request");
+const { resourceLimits } = require("worker_threads");
+const apiBase = "https://restcountries.com/v3.1/all";
+let countriesNames = []
+request.get (apiBase,(err,res,body) =>{
+    // console.log(body);
+    let countries = JSON.parse(body);
+    // console.log(countries[0].name.common);
+    for (var i = 0;i<countries.length;i++){
+        var b = countries[i].name.common
+        countriesNames.push(b)
+        console.log(countriesNames);
+        
+    }
+})
+
+
+console.log("request");
