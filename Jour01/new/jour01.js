7c5128ea3a3d0b78efd5832a499d80fa5d1236b1
Ismail51 <dassler-ben@hotmail.fr>
2022-02-17 16:32:50 +0100

    entrainement jour1

---
+++ Jour01/new/jour01.js
@@ -0,0 +1,77 @@
+// // 01 - Hello world
+// console.log("Hello world !");
+
+// // 02 - String
+// var test = 'My name is ismail'
+// console.log(test);
+
+// //03 name
+// var name = 'ismail'
+// console.log('Nice to meet you ' + name);
+
+// //04 string lenght
+// var testLenght = "I'm very long !";
+// console.log(testLenght);
+
+// //05 replace
+// var food = "croissant is meh";
+// food = food.replace("meh","so good");
+// console.log(food);
+
+// //06 up and down
+// var basic = "this is cool";
+// var basicUp = basic.toUpperCase();
+// var basicDown= basic.toLowerCase();
+// console.log(basic + " " + basicUp + " " + basicDown);
+
+// //07 split
+// var word = "banana";
+// var letters = word.split("");
+// console.log(letters);
+
+// //08 template
+// var age = "27";
+// var template = "I'm __ years old";
+// template = `I'm ${age} years`;
+// console.log(template);
+
+// 01 Hello World
+// console.log("Hello World!")
+
+// 02 String
+// let test;
+// test = "my name is Sam"
+// console.log(test);
+
+// 03 Concaténation
+// let name; 
+// name = "Sam"
+// console.log(`Nice to meet you ${name}`)
+
+// 04 String Length
+// let testLength;
+// testLength ="I'm very long"
+// console.log(testLength)
+
+// 05 Replace
+// let food = "croissant is meh"
+// food = food.replace("meh","so good")
+// console.log(food)
+
+// 06 Up and Down
+// let basic = "This is cool";
+// let basicUp = basic.toUpperCase();
+// let basicDown = basic.toLowerCase();
+// console.log(basic, basicUp, basicDown)
+
+// 07 Split
+// let word = "banana";
+// let letters = word.split("");
+// console.log(letters)
+
+// 08 Template
+const age = "27";
+let template = `I'm ${age} years old`;
+console.log(template)
+
+
