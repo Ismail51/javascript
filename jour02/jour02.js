41c19bbd1e98e047d912d3a2a94dce5e6012048a
Ismail51 <dassler-ben@hotmail.fr>
2022-02-23 09:34:50 +0100

    entr 2

--- jour02/jour02.js
+++ jour02/jour02.js
@@ -1,75 +1,159 @@
-// 01 number
-var integer = 102;
-var float = 13.9;
-console.log(integer, float);
+// // 01 number
+// var integer = 102;
+// var float = 13.9;
+// console.log(integer, float);
 
-// 02 convert
-var basic = 34;
-var stringified = (basic);
-console.log(stringified);
+// // 02 convert
+// var basic = 34;
+// var stringified = (basic);
+// console.log(stringified);
 
-// 03 round
-var num = 1.5;
-var rounded = Math.round(num);
-console.log(rounded);
+// // 03 round
+// var num = 1.5;
+// var rounded = Math.round(num);
+// console.log(rounded);
 
-// 04 arithmétique
-var test = 12;
-var bis = 5;
-console.log(test + bis);
-console.log(test * bis);
-console.log(test - bis);
-console.log(test % bis);
-console.log(test / bis);
-console.log(test ** bis);
-
-
-// 05 comparaison
-var test = 143;
-var bis = 219;
-console.log(test > bis);
-console.log(test < bis);
-console.log(test >= bis);
-console.log(test <= bis);
-console.log(test == bis);
-console.log(test === bis);
-console.log(test != bis);
-console.log(test !== bis);
-
-//06 condition 
-var limit = 50;
-var score = 45;
-if (score >= limit) {
-console.log("ok good!");  
-}
-else {
-    console.log("Oh nooo...")
-};
-
-//07 condition II
-var password = "azer";
-if (password.length > 5) {
-    console.log("The password is secure")
-}
-else{
-    console.log("wsh");
-}
-
-//08 condition III
-if (score >= limit  && password.length > 5)
-{
-    console.log("Everything is good")
-} else if (score >= limit  || password.length > 5) {
-    console.log("Something is good")
-} 
-else{ console.log("Nothing is good");
-}
+// // 04 arithmétique
+// var test = 12;
+// var bis = 5;
+// console.log(test + bis);
+// console.log(test * bis);
+// console.log(test - bis);
+// console.log(test % bis);
+// console.log(test / bis);
+// console.log(test ** bis);
 
 
+// // 05 comparaison
+// var test = 143;
+// var bis = 219;
+// console.log(test > bis);
+// console.log(test < bis);
+// console.log(test >= bis);
+// console.log(test <= bis);
+// console.log(test == bis);
+// console.log(test === bis);
+// console.log(test != bis);
+// console.log(test !== bis);
+
+// //06 condition 
+// var limit = 50;
+// var score = 45;
+// if (score >= limit) {
+// console.log("ok good!");  
+// }
+// else {
+//     console.log("Oh nooo...")
+// };
+
+// //07 condition II
+// var password = "azer";
+// if (password.length > 5) {
+//     console.log("The password is secure")
+// }
+// else{
+//     console.log("wsh");
+// }
+
+// //08 condition III
+// if (score >= limit  && password.length > 5)
+// {
+//     console.log("Everything is good")
+// } else if (score >= limit  || password.length > 5) {
+//     console.log("Something is good")
+// } 
+// else{ console.log("Nothing is good");
+// }
 
 
-//bonus
- var random = Math.random6;
+
+
+// //bonus
+//  var random = Math.random6;
+//  if (random === 6) {
+//      console.log("Yes I win!")
+//  }
+//  else { console.log("So close")
+
+//  }
+
+
+
+// 01 Number
+
+// let integer = 102;
+// let float = 13.9;
+// console.log(integer, float)
+
+
+// 02 Convert 
+
+// let basic = 34;
+// let stringifield = basic.toString();
+// console.log(stringifield)
+
+//  03 Round
+//  let num = 1.5
+//  let rounded = Math.round(num)
+//  console.log(rounded);
+
+ // 04 Arithmétique
+//  let test = 12;
+//  let bis = 5;
+//  console.log(test + bis)
+//  console.log(test - bis)
+//  console.log(test * bis)
+//  console.log(test / bis)
+//  console.log(test ** bis)
+//  console.log(test % bis)
+
+ // 5 Comparaison 
+
+//  let test = 143
+//  let bis = 219
+//  console.log(test > bis)
+//  console.log(test < bis)
+//  console.log(test >= bis)
+//  console.log(test <= bis)
+//  console.log(test === bis)
+//  console.log(test != bis)
+//  console.log(test !== bis)
+
+// 06 Condition
+
+// let limit = 50
+// let score = 30
+// if(score >= limit){
+//     console.log("ok good");
+// }
+// else{
+//     console.log("oh nooo....");
+// }
+
+// // 07 Condition II
+
+// let password = "azerty"
+// if(password.length > 3 ){
+//     console.log("The password is secure");
+// }
+
+
+// 08 
+
+// if(password.length > 5 && score >= limit){
+//     console.log("everything is good");
+// }
+// else if(password.length > 9 || score >= limit){
+//     console.log("something is good");
+// }
+// else{
+//     console.log("nothing is good");
+// }
+
+// Bonus
+
+ var random = Math.ceil(Math.random() * 6);
+ console.log(random)
  if (random === 6) {
      console.log("Yes I win!")
  }
